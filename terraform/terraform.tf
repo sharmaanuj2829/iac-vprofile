0038fcaeda2f5e6c74a585adbaa5ec26177051f5
sharmaanuj2829 <sharmaanuj2829@gmail.com>
2026-04-15 10:46:51 +0530

    NewlyEdited

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,28 +1,30 @@
 terraform {
+  required_version = "~> 1.6"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.34"
+      version = "~> 5.40"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.6"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = "~> 2.25"
     }
   }
 
@@ -31,9 +33,4 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
-
-  required_version = "~> 1.6.3"
 }
-##
-##
-##
